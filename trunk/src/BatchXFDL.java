fccfdd964a01e0f631b737b1253abcb337d2fc3c
andrew.nortrup@gmail.com <andrew.nortrup@gmail.com@e7ee94ce-7a64-e645-d331-44d3a6cc62d7>
2011-05-22 20:44:01 +0000

    Further updates of the form renderer.  Starting to decouple the renderer from the FormPanel so that once the components are put into the panel they are not maintained by the renderer, except through a reference to the form itself.

--- trunk/src/BatchXFDL.java
+++ trunk/src/BatchXFDL.java
@@ -6,8 +6,6 @@ public class BatchXFDL {
 	public static void main(String[] args) {
 		 
 			try {	
-				
-				
 				//Display Main Window
 				MainWindowControl controller = new MainWindowControl();
 				MainWindow view = new MainWindow(controller);
--- trunk/src/FormPanel.java
+++ trunk/src/FormPanel.java
@@ -1,6 +1,9 @@
+
+import java.awt.Component;
 import java.awt.Graphics;
 import java.awt.Rectangle;
 import java.util.ArrayList;
+import java.util.HashMap;
 
 import javax.swing.JPanel;
 
@@ -22,9 +25,12 @@ public class FormPanel extends JPanel {
 	private static final long serialVersionUID = -5408728614801306159L;
 	
 	private ArrayList<Rectangle> lines;
+	private HashMap<Component, String> components;
 	
 	public FormPanel() {
 		lines = new ArrayList<Rectangle>();
+		components = new HashMap<Component, String>();
+		
 	}
 	
 	public void addLine(Rectangle line) {
@@ -38,15 +44,48 @@ public class FormPanel extends JPanel {
 		super.paintComponent(g);
 		 
 		for(int x = 0; x<lines.size(); x++) {
-			g.drawRect(lines.get(x).x, 
-					lines.get(x).y, 
-					lines.get(x).width, 
-					lines.get(x).height);
 			g.fillRect(lines.get(x).x, 
 					lines.get(x).y, 
 					lines.get(x).width, 
 					lines.get(x).height);
 		}
 	}
+	
+	/**
+	 * Overrides the base class implementation of add to set the panels width 
+	 * and height as the components are added in order to keep the size large 
+	 * enough for all components.
+	 */
+	public Component add(Component component) {
+		
+		int width = this.getWidth();
+		int height = this.getHeight();
+		
+		if(component.getX() + component.getWidth() > width) {
+			width = component.getX() + component.getWidth();
+		}
+		if(component.getY() + component.getHeight() > height) {
+			height = component.getY() + component.getHeight();
+		}
+		
+		this.setSize(width, height);
+		this.setPreferredSize(this.getSize());
+		
+		return super.add(component);
+	}
+	
+	/**
+	 * Adds a component with the specified SID
+	 * @param component Component to be added
+	 * @param sid SID of the component
+	 * @return Component to be added.
+	 */
+	public Component add(Component component, String sid) {
+		
+		components.put(component, sid);
+		
+		return super.add(component);
+	}
+
 
 }
